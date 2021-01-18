a3e660abfea0fdd9d445b912adf4ff2faacabfa6
5751mn <edi.eastfield99@gmail.com>
2021-01-18 22:47:17 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/mysimplecleanarchitecture/data/MessageDataSource.kt
@@ -0,0 +1,7 @@
+package com.dicoding.picodiploma.mysimplecleanarchitecture.data
+
+import com.dicoding.picodiploma.mysimplecleanarchitecture.domain.MessageEntity
+
+class MessageDataSource : IMessageDataSource {
+    override fun getMessageFromSource(name: String) = MessageEntity("Hello $name! Welcome to Clean Architecture")
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/mysimplecleanarchitecture/data/MessageRepository.kt
@@ -0,0 +1,8 @@
+package com.dicoding.picodiploma.mysimplecleanarchitecture.data
+
+import com.dicoding.picodiploma.mysimplecleanarchitecture.domain.IMessageRepository
+import com.dicoding.picodiploma.mysimplecleanarchitecture.domain.MessageEntity
+
+class MessageRepository(private val messageDataSource: IMessageDataSource) : IMessageRepository {
+    override fun getWelcomeMessage(name: String) = messageDataSource.getMessageFromSource(name)
+}
